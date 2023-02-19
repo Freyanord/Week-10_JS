29acf0ad9437dc469599a68cf8de30247ecfc3ca
Freyanord <annabashina135@gmail.com>
2023-02-19 16:05:25 +0300

    Correct

--- task2.js
+++ task2.js
@@ -6,3 +6,12 @@ function changeImg2() {
     let image = document.getElementById('cat');
     image.src = "images/cat1.jpg";
 }
+
+    let year = prompt ('Введите год')
+    if(((year % 4 == 0) && (year % 100 != 0)) && (year % 400 ==0)){
+        alert('Високосный');
+    }
+    else {
+        alert('Не високосный');
+    }
+
