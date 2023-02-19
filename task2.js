c0fa555f4414582f2350f67247febe124a7aaa2b
Freyanord <annabashina135@gmail.com>
2023-02-19 14:30:58 +0300

    Correct

---
+++ task2.js
@@ -0,0 +1,8 @@
+function changeImg() {
+    let image = document.getElementById('cat');
+    image.src = "images/cat2.jpg";
+}
+function changeImg2() {
+    let image = document.getElementById('cat');
+    image.src = "images/cat1.jpg";
+}
