70c9f832ae6cdd00bbcbb314f8afff194cf8630f
Freyanord <annabashina135@gmail.com>
2023-05-02 19:56:38 +0300

    Correct

--- task2.js
+++ task2.js
@@ -1,17 +1,17 @@
+const image = document.getElementById('cat');
+
 function changeImg() {
-    let image = document.getElementById('cat');
     image.src = "images/cat2.jpg";
 }
 function changeImg2() {
-    let image = document.getElementById('cat');
     image.src = "images/cat1.jpg";
 }
 
-    // let year = prompt ('Введите год')
-    // if(((year % 4 == 0) && (year % 100 != 0)) && (year % 400 ==0)){
-    //     alert('Високосный');
-    // }
-    // else {
-    //     alert('Не високосный');
-    // }
+    const year = prompt ('Введите год')
+    if(((year % 4 == 0) && (year % 100 != 0)) && (year % 400 ==0)){
+        alert('Високосный');
+    }
+    else {
+        alert('Не високосный');
+    }
 
